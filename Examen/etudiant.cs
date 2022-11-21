228811ba7a8ad79c654a666d13bf803dcb7c4ad0
Nouhayla <nouhayla.ennadri@usmba.ac.ma>
2022-11-21 22:51:17 +0100

    ajout d'etudiant

--- Examen/etudiant.cs
+++ Examen/etudiant.cs
@@ -7,6 +7,20 @@ using System.Threading.Tasks;
 namespace Examen
 {
     internal class etudiant
+
     {
+        public int CIN { get; set; }
+        public string nom { get; set; }
+        public string Prenom { get; set; }
+        private List<Reponse> reponses = new List<Reponse>();
+
+        public etudiant() { }
+        public etudiant(int id, string nom, string Prenom, List<Reponse> reponses)
+        {
+            this.CIN = CIN;
+            this.nom = nom;
+            this.Prenom = Prenom;
+            this.reponses = new List<Reponse>(reponses);
+        }
     }
 }
